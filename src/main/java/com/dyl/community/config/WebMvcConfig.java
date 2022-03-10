6d023e47baec29e1d07fdbb8a92d869aff437896
xinghe <1329749225@qq.com>
2022-03-10 23:41:50 +0800

    day06 second commit(完成显示系统消息详情功能，重构了首页的消息总数)

--- src/main/java/com/dyl/community/config/WebMvcConfig.java
+++ src/main/java/com/dyl/community/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.dyl.community.config;
 import com.dyl.community.controller.interceptor.AlphaInterceptor;
 import com.dyl.community.controller.interceptor.LoginRequiredInterceptor;
 import com.dyl.community.controller.interceptor.LoginTicketInterceptor;
+import com.dyl.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -23,6 +24,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -33,6 +37,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
