35a53f5222090781e6334fc316b891a8af3eca5a
xinghe <1329749225@qq.com>
2022-03-13 17:04:55 +0800

    day09 third commit(完成了上传头像和分享长图到七牛云服务器功能)

--- src/main/java/com/dyl/community/controller/ShareController.java
+++ src/main/java/com/dyl/community/controller/ShareController.java
@@ -46,11 +46,13 @@ public class ShareController implements CommunityConstant {
     @Value("${wk.pdf.storage}")
     private String wkPdfStorage;
 
+    @Value("${qiniu.bucket.share.url}")
+    private String shareBucketUrl;
+
     @RequestMapping(path = "/share", method = RequestMethod.GET)
     @ResponseBody
     public String share(String htmlUrl) {
         // 文件名
-//        String fileName = CommunityUtil.generateUUID().replace("-", "");
         String fileName = CommunityUtil.generateUUID();
 
         // 异步生成长图
@@ -63,7 +65,8 @@ public class ShareController implements CommunityConstant {
 
         // 返回访问路径
         Map<String, Object> map = new HashMap<>();
-        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+//        map.put("shareUrl", domain + contextPath + "/share/image/" + fileName);
+        map.put("shareUrl", shareBucketUrl + "/" + fileName);
 
         return CommunityUtil.getJSONString(0, null, map);
     }
